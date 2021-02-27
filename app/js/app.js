cc89bcb27cf2faac9e15fc7a36e535ec1b988129
Prajwal Poojary <prajwalpoojary67@gmail.com>
2021-02-27 22:23:20 +0530

    carousel from scratch - mobile version
    should work on updating it to the desktop version as well

--- app/js/app.js
+++ app/js/app.js
@@ -74,3 +74,88 @@ function showError() {
   // Set the styling appropriately
   emailError.className = 'error active';
 }
+
+
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+const prevBtn = document.querySelector('.carousel__button--left');
+const nextBtn = document.querySelector('.carousel__button--right');
+const dotNav = document.querySelector('.carousel__nav');
+const dots = Array.from(dotNav.children);
+
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+// Arrange the slides next to one another
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + 'px';
+}
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = `translateX(-${targetSlide.style.left})`;
+  currentSlide.classList.remove('current-slide');
+  targetSlide.classList.add('current-slide');
+}
+
+const updateDots = (currentDot, targetDot) => {
+  currentDot.classList.remove('current-slide');
+  targetDot.classList.add('current-slide');
+};
+
+const hideShowArrows = (slides, prevBtn, nextBtn, targetIndex) => {
+  if(targetIndex === 0) {
+    prevBtn.classList.add('is-hidden');
+    nextBtn.classList.remove('is-hidden');
+  } else if(targetIndex === slides.length - 1) {
+    prevBtn.classList.remove('is-hidden');
+    nextBtn.classList.add('is-hidden');
+  } else {
+    prevBtn.classList.remove('is-hidden');
+    nextBtn.classList.remove('is-hidden');
+  }
+};
+
+
+// When I click left, move slides to the left
+prevBtn.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+  const prevSlide = currentSlide.previousElementSibling;
+  const currentDot = dotNav.querySelector('.current-slide');
+  const prevDot = currentDot.previousElementSibling;
+  const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
+  moveToSlide(track, currentSlide, prevSlide);
+  updateDots(currentDot, prevDot);
+  hideShowArrows(slides, prevBtn, nextBtn, prevIndex);
+});
+
+// When I click right, move slides to the right
+nextBtn.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+  const nextSlide = currentSlide.nextElementSibling;
+  const currentDot = dotNav.querySelector('.current-slide');
+  const nextDot = currentDot.nextElementSibling;
+  const nextIndex = slides.findIndex(slide => slide === nextSlide);
+
+  moveToSlide(track, currentSlide, nextSlide);
+  updateDots(currentDot, nextDot);
+  hideShowArrows(slides, prevBtn, nextBtn, nextIndex);
+});
+
+// When I click the nav indicator, move to that slide
+dotNav.addEventListener('click', (e) => {
+  // what indicator clicked on?
+  const targetDot = e.target.closest('.carousel__indicator');
+
+  if(!targetDot) return;
+
+  const currentSlide = track.querySelector('.current-slide');
+  const currentDot = dotNav.querySelector('.current-slide');
+  const targetIndex = dots.findIndex(dot => dot === targetDot);
+  const targetSlide = slides[targetIndex];
+
+  moveToSlide(track, currentSlide, targetSlide);
+  updateDots(currentDot, targetDot);
+  hideShowArrows(slides, prevBtn, nextBtn, targetIndex);
+});
